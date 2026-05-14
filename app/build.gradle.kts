4a2c67918b40d106e48a12486999a0300555753c
Kenneth Lee <kenneth-lee-2012@qq.com>
2026-05-14 16:44:53 +0800

    版本升级到0.14
    Signed-off-by: Kenneth Lee <kenneth-lee-2012@qq.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "personal.kenny.platalea"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "0.13"
+        versionCode = 14
+        versionName = "0.14"
     }
     signingConfigs {
         create("release") {
